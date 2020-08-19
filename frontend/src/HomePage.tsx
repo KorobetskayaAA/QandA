2c8bb5c9e1f29a427012b484fc1216e5c4e07139
А.Коробецкая <kornast@yandex.ru>
2020-08-19 21:08:33 +0400

    Page and title template

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -4,35 +4,39 @@ import { css, jsx } from '@emotion/core';
 import { PrimaryButton } from './Styles';
 import { QuestionList } from './QuestionList';
 import { getUnansweredQuestions } from './QuestionsData';
+import { Page } from './Page';
+import { PageTitle } from './PageTitle';
 
 export const HomePage = () => (
-  <div
-    css={css`
-      margin: 50px auto 20px auto;
-      padding: 30px 20px;
-      max-width: 600px;
-    `}
-  >
+  <Page>
     <div
       css={css`
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
+        margin: 50px auto 20px auto;
+        padding: 30px 20px;
+        max-width: 600px;
       `}
     >
-      <h2
+      <div
         css={css`
-          font-size: 15px;
-          font-weight: bold;
-          margin: 10px 0px 4px;
-          text-align: center;
-          text-transform: uppercase;
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
         `}
       >
-        Unanswered Questions
-      </h2>
-      <PrimaryButton>Ask a question</PrimaryButton>
+        <h2
+          css={css`
+            font-size: 15px;
+            font-weight: bold;
+            margin: 10px 0px 4px;
+            text-align: center;
+            text-transform: uppercase;
+          `}
+        >
+          <PageTitle>Unanswered Questions</PageTitle>
+        </h2>
+        <PrimaryButton>Ask a question</PrimaryButton>
+      </div>
+      <QuestionList data={getUnansweredQuestions()} />
     </div>
-    <QuestionList data={getUnansweredQuestions()} />
-  </div>
+  </Page>
 );
