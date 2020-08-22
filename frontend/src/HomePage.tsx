6243e7ad8e6145638dfd757dbcc31e3ab452feaa
А. Коробецкая <kornast@yandex.com>
2020-08-22 20:30:01 +0300

    Loading state for questions list

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
 import { PrimaryButton } from './Styles';
@@ -6,42 +5,53 @@ import { QuestionList } from './QuestionList';
 import { getUnansweredQuestions, QuestionData } from './QuestionsData';
 import { Page } from './Page';
 import { PageTitle } from './PageTitle';
+import { useEffect, useState } from 'react';
 
 /*const renderQuestion = (question: QuestionData) => <div>{question.title}</div>;*/
 
-export const HomePage = () => (
-  <Page>
-    <div
-      css={css`
-        margin: 50px auto 20px auto;
-        padding: 30px 20px;
-        max-width: 600px;
-      `}
-    >
+export const HomePage = () => {
+  const [questions, setQuestions] = useState<QuestionData[] | null>(null);
+  const [questionsLoading, setQuestionsLoading] = useState(true);
+  useEffect(() => {
+    const doGetUnansweredQuestions = async () => {
+      const unansweredQuestions = await getUnansweredQuestions();
+      setQuestions(unansweredQuestions);
+      setQuestionsLoading(false);
+    };
+    doGetUnansweredQuestions();
+  }, []);
+  return (
+    <Page>
       <div
         css={css`
-          display: flex;
-          align-items: center;
-          justify-content: space-between;
+          margin: 50px auto 20px auto;
+          padding: 30px 20px;
+          max-width: 600px;
         `}
       >
-        <h2
+        <div
           css={css`
-            font-size: 15px;
-            font-weight: bold;
-            margin: 10px 0px 4px;
-            text-align: center;
-            text-transform: uppercase;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
           `}
         >
           <PageTitle>Unanswered Questions</PageTitle>
-        </h2>
-        <PrimaryButton>Ask a question</PrimaryButton>
+          <PrimaryButton>Ask a question</PrimaryButton>
+        </div>
       </div>
-      <QuestionList
-        data={getUnansweredQuestions()}
-        /*renderItem={renderQuestion}*/
-      />
-    </div>
-  </Page>
-);
+      {questionsLoading ? (
+        <div
+          css={css`
+            font-size: 16px;
+            font-style: italic;
+          `}
+        >
+          Loading...
+        </div>
+      ) : (
+        <QuestionList data={questions || []} />
+      )}
+    </Page>
+  );
+};
