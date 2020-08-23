b20dd77fd95752bcd133283e1117f6a64761db32
А. Коробецкая <kornast@yandex.com>
2020-08-23 17:15:48 +0300

    Basic routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,19 +4,28 @@ import { HomePage } from './HomePage';
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
 import { fontFamily, fontSize, gray2 } from './Styles';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { AskPage } from './AskPage';
+import { SearchPage } from './SearchPage';
+import { SignInPage } from './SignInPage';
 
 function App() {
   return (
-    <div
-      css={css`
-        font-family: ${fontFamily};
-        font-size: ${fontSize};
-        color: ${gray2};
-      `}
-    >
-      <Header />
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div
+        css={css`
+          font-family: ${fontFamily};
+          font-size: ${fontSize};
+          color: ${gray2};
+        `}
+      >
+        <Header />
+        <Route exact path="/" component="{HomePage}" />
+        <Route path="/search" component="{SearchPage}" />
+        <Route path="/ask" component="{AskPage}" />
+        <Route path="/signin" component="{SignInPage}" />
+      </div>
+    </BrowserRouter>
   );
 }
 
