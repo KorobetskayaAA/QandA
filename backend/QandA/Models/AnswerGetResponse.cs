b23333dd49586f7918dad98f767892e686d8a683
А. Коробецкая <kornast@yandex.ru>
2020-08-31 11:38:26 +0400

    Writing data using Dapper

---
+++ backend/QandA/Models/AnswerGetResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace QandA.Models
+{
+    public class AnswerGetResponse
+    {
+        public int AnswerId { get; set; }
+        public int QuestionId { get; set; }
+        public string Content { get; set; }
+        public string Username { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
